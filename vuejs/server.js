a92a5d91bfa5a9a7a29a86910586498fd086e493
FlwRnt <florent.auplat@etu.univ-tours.fr>
2017-11-06 13:19:55 -0500

    add website

---
+++ vuejs/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path');
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("access-control-allow-headers", "origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin");
+  res.header("Content-Type", "application/json");
+  next();
+});
+
+let ultrasonic = require('./API/ultrasonic')
+app.use('/api', ultrasonic)
+
+app.get('/api/test', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+});
