078cc2f8313747bcc8c43139b234aa3f546231a2
FlwRnt <florent.auplat@etu.univ-tours.fr>
2017-11-06 19:48:19 -0500

    create lights API

--- vuejs/API/light.js
+++ vuejs/API/light.js
@@ -4,20 +4,18 @@ let child_proc  = require('child_process');
 let db          = require('mongojs')('mongodb://dreamteam:domotique@ds133311.mlab.com:33311/smartflat')
 
 router.get('/lights', (req, res) => {
-  let script = child_proc.spawn('python', ['./scripts/moc_ultrasonic.py'])
-  
-  script.stderr.on('data', (data) => {
-    console.log('stderr: ${data}');
-  });
-  
-  res.send('coucou')
-})
-
-router.get('/ultrasonics', (req, res) => {
-  db.ultrasonics.find().toArray((err, items) => {
+  db.lights.find().toArray((err, items) => {
     if (err) console.log(err)
     res.json(items)
   })
+})
+
+router.get('/lights/status', (req, res) => {
+
+})
+
+router.post('/lights/:id/:status', (req, res) => {
+  
 })
 
 module.exports = router
