8102dd80dbc1274a9a1292c4375dbd99091ca216
FlwRnt <florent.auplat@etu.univ-tours.fr>
2017-12-02 13:51:18 -0500

    add window button & moc luminosity

--- smartflat/API/refresh.js
+++ smartflat/API/refresh.js
@@ -4,8 +4,7 @@ let child_proc  = require('child_process');
 
 router.post('/refresh/:effector', (req, res) => {
   // todo: call manager to reload data & take decision
-  let effector = req.param.effector
-  
+  let effector = req.param.effector  
   
 })
 
--- smartflat/scripts/moc_buttons.py
+++ smartflat/scripts/moc_buttons.py
@@ -6,11 +6,13 @@ from pymongo import MongoClient
 
 TV = "TV"
 FURNACE = "Furnace"
+WINDOW = "Window"
 UNKNOWN = "unknown"
 ON = "on"
 OFF = "off"
 PIN_TV = 20
 PIN_FURNACE = 8
+PIN_WINDOW = 10
 
 # get database
 client = MongoClient('mongodb://dreamteam:domotique@ds133311.mlab.com:33311/smartflat')
@@ -20,11 +22,10 @@ pin = int(sys.argv[1])
 stat = int(sys.argv[2])
 
 status = OFF if stat == 0 else ON
-location = TV if pin == PIN_TV else FURNACE if pin == PIN_FURNACE else UNKNOWN
+location = TV if pin == PIN_TV else FURNACE if pin == PIN_FURNACE else WINDOW if pin == PIN_WINDOW else UNKNOWN
 date = str(datetime.datetime.utcnow()).replace(" ", "T")
 
 button = {
-  "pin": pin,
   "location": location,
   "status": status,
   "manual": True,
